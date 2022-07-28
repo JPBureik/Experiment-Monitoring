4f53eeb01b5b1ce09ee549a51e74499576afdebe
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-07-28 16:50:12 +0200

    Fix typo

--- src/expmonitor/classes/webcam.py
+++ src/expmonitor/classes/webcam.py
@@ -11,7 +11,7 @@ Created on Thu Jul 28 15:15:51 2022
 
 # Local imports:
 from expmonitor.classes.sensor import Sensor
-from expmonitor.utilities import img_proc
+from expmonitor.utilities.img_proc import img_proc
 
 
 class Webcam(Sensor):
