5ccc8609d88768f22540aa4fec519f8438ed620c
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-07-29 12:20:43 +0200

    Minor bugfix

--- src/expmonitor/utilities/img_proc.py
+++ src/expmonitor/utilities/img_proc.py
@@ -80,7 +80,7 @@ def img_proc(savepath):
         
     if '-e' in raw_res:
         
-        raw_res = raw_res.replace('-e', 'e')
+        raw_res = raw_res.replace('—', '')
     
     val = float(raw_res)
     
